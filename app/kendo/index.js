31e966042f617e674a16447fbb799ebf847788bf
Georgi Krustev <ggkrustev@yahoo.com>
2016-01-04 18:25:48 +0200

    Add Kendo UI Core to test site

---
+++ app/kendo/index.js
@@ -0,0 +1,12 @@
+//load scripts
+require('jquery');
+require('kendo-ui-core/src/kendo.core');
+require('kendo-ui-core/src/kendo.fx');
+require('kendo-ui-core/src/kendo.data');
+require('kendo-ui-core/src/kendo.popup');
+require('kendo-ui-core/src/kendo.list');
+require('kendo-ui-core/src/kendo.dropdownlist');
+
+//load styles
+require('kendo-ui-core/dist/styles/web/kendo.common.core.css');
+require('kendo-ui-core/dist/styles/web/kendo.default.css');
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var webpack = require('webpack');
 var path = require('path');
 var nodeModulesPath = path.resolve(__dirname, 'node_modules');
 var buildPath = path.resolve(__dirname, 'public', 'build');
+var kendoPath = path.resolve(__dirname, 'app', 'kendo', 'index.js');
 var mainPath = path.resolve(__dirname, 'app', 'main.js');
 
 var config = {
@@ -11,6 +12,8 @@ var config = {
         'webpack/hot/dev-server',
         // The script refreshing the browser on none hot updates
         'webpack-dev-server/client?http://localhost:8080',
+        //Kendo UI Core,
+        kendoPath,
         // Our application
         mainPath
     ],
@@ -27,9 +30,17 @@ var config = {
         }, {
             test: /\.css$/,
             loader: 'style!css'
+        }, {
+            test: /\.gif$|\.png$/, loader: 'file-loader'
         }]
     },
-    plugins: [new webpack.HotModuleReplacementPlugin()]
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.ProvidePlugin({
+            jQuery: 'jquery',
+            $: 'jquery'
+        })
+    ]
 };
 
 module.exports = config;
